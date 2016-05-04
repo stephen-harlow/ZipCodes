fc5ff04b6ecb19a96f7ebd4b999fa021c2b9af08
stephen-harlow <srh150030@utdallas.edu>
2016-05-04 16:33:40 -0500

    Fix Paths 6

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ app.use(publicPath);
 
 // place your handlers here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 // place your handlers here
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 
